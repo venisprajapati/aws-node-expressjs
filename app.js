6431c4237c67eb83f706cfbd0893282acdc4ac9b
Venis Prajapati <59964517+venisprajapati@users.noreply.github.com>
2021-06-17 23:27:39 +0530

    updated version 1.2.0

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send('<h1>Express Demo App</h1> <h4>Message: Success</h4> <p>version: 1.0.0</p>');
+    res.send('<h1>Express Demo App</h1> <h4>Message: Success</h4> <p>version: 1.2.0</p>');
 });
 
 app.get('/products', (req, res) => {
